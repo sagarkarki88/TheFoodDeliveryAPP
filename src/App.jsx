7dd4ae7adcd649635d27e501e8651e4e19e6848f
sagar <sagarkarki88@gmail.com>
2023-06-28 23:34:27 +1000

    adding meal summary banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Fragment, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
+/* ******************App Components************************* */
 import Header from "./components/Layout/Header";
+import MealsSummaryBanner from "./components/Meals/MealsSummaryBanner";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <MealsSummaryBanner />
     </Fragment>
   );
 }
---
+++ src/components/Meals/MealsSummaryBanner.jsx
@@ -0,0 +1,22 @@
+import classes from "./MealsSummaryBanner.module.css";
+export default function MealSummaryBanner() {
+  return (
+    <>
+      <section className={classes.summary}>
+        <h2>Lorem ipsum dolor, sit amet consectetur adipisicing </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil
+          commodi asperiores cum corporis quis debitis, adipisci beatae
+          inventore quaerat corrupti, perferendis fugiat voluptas. Dolorem nemo
+          impedit recusandae soluta non.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta
+          excepturi assumenda quo culpa accusantium consectetur eius, saepe
+          eaque, fugit quibusdam similique eos dolorum facere harum, enim nemo
+          consequatur aspernatur maxime!
+        </p>
+      </section>
+    </>
+  );
+}
