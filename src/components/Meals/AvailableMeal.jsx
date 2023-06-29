8d72045a7a5015a8ba3f63e966cb25e6de9cc0ae
sagar <sagarkarki88@gmail.com>
2023-06-29 20:12:34 +1000

    adding menulist unstyled

---
+++ src/components/Meals/AvailableMeal.jsx
@@ -0,0 +1,43 @@
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+
+import classes from "./AvailableMeal.module.css";
+export default function AvailableMeal() {
+  return (
+    <>
+      <section className={classes.meals}>
+        <ul>
+          {DUMMY_MEALS.map((each) => (
+            <li>
+              {each.name} {each.description} {each.price}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+}
