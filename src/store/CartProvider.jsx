8db5925883031583d1856de8369f6ef4c4ad5f08
sagar <sagarkarki88@gmail.com>
2023-07-05 11:57:09 +1000

    adding reducer to manage the  provider states of context

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,13 +1,40 @@
 import CartContext from "./Cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartSateReducerFn = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItem = state.items.concat(action.item);
+    const updateAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return { items: updatedItem, totalAmount: updateAmount };
+  }
+  if (action.type === "REMOVE") {
+  }
+  return defaultCartState;
+};
 
 export default function CartProvider(props) {
-  const addItemHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartSateReducerFn,
+    defaultCartState
+  );
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
 
-  const removeItemHandler = (id) => {};
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
